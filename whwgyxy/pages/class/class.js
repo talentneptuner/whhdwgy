fba0925ab1d186b003668f40b1d9ef625a1afe1b
neptuner <1141764044@qq.com>
2018-08-28 15:05:28 +0800

    end_2
    添加了图片保存

--- whwgyxy/pages/class/class.js
+++ whwgyxy/pages/class/class.js
@@ -1,12 +1,13 @@
 var g_class;
-var show_desc = true;
-var show_course = false;
-var show_image = false;
-var show_prize = false;
+var show_desc = true
+var show_course = false
+var show_image = false
+var show_prize = false
 var desc_class = 'nav-item-active'
 var image_calss = 'nav-item'
 var prize_class = 'nav-item'
 var course_class = 'nav-item'
+var imgurl = ''
 // pages/class/class.js
 Page({
 
@@ -30,6 +31,7 @@ Page({
         'content-type': 'application/json'
       },
       success: function(res){
+        g_class = res.data
         that.setData({
           info:res.data,
           show_desc: show_desc,
@@ -134,8 +136,59 @@ Page({
       image_class: image_calss,
       prize_class: prize_class
     })
-  }
+  },
 
+  saveAction: function(res){
+      var id = res.currentTarget.dataset.id
+      // console.log(g_class.images)
+      for (let i = 0 ;i< g_class.images.length;i++){
+        if(g_class.images[i].id == id){
+          imgurl = g_class.images[i].image
+          break
+        }
+      }
+      //console.log(imgurl)
+      wx.showActionSheet({
+        itemList: ['保存图片',],
+        success : function(tap){
+          if(tap.tapIndex === 0){
+            wx.downloadFile({
+              url:imgurl,
+              success:function(res){
+               // console.log(res)
+                wx.saveImageToPhotosAlbum({
+                  filePath: res.tempFilePath,
+                  success:function(res){
+                    wx.showToast({
+                      title: '保存成功',
+                    })
+                  },
+                  fail:function(res){
+                    wx.showToast({
+                      title: '保存失败',
+                    })
+                  }
+                })
+              }
+            })
+          }
+        }
+      })
+  },
+
+  previewImages:function(res){
+    var id = res.currentTarget.dataset.id
+    // console.log(g_class.images)
+    for (let i = 0; i < g_class.images.length; i++) {
+      if (g_class.images[i].id == id) {
+        imgurl = g_class.images[i].image
+        break
+      }
+    }
+    wx.previewImage({
+      urls: [imgurl,],
+    })
+  }
 
  
 })
